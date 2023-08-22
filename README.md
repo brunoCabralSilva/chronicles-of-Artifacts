fd14a5abbf5aab4fee994b160d6b272059876c38
bruno <bruno.cabral.silva2018@gmail.com>
2023-08-22 12:36:52 -0300

    gitignore

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 <h1 align="center">Chronicles of Artifacts</h1>
 
-<p align="center">Aplicação criada para praticar a aplicação do React Redux no Typescript.</p>
+<p align="center">
+    
+</p>
 
 <div align="center">
   <img align="center" src="./src/images/schema.png" alt= "Esquema do Banco de dados do projeto" />
--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
   public static void main(String[] args) throws Exception {
 
     ArmorsControl armorsControl = new ArmorsControl(new ArmorsService(new ArmorsModel()));
-    // armorsControl.getAllArmors();
+    armorsControl.getAllArmors();
     // armorsControl.insertArmor("Armadura de ossos", 2, 0, 0, "traje");
     // armorsControl.updateArmor(21, "Armadura de lombras", 2, 0, 0, "placas");
     // armorsControl.removeArmor("Armadura de lombras");
